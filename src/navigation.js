d9c52afbdfc203b2ad1e5c6c6ecb1cc62742e741
Santiago Bernal <exactlimon@gmail.com>
2024-07-11 20:48:49 -0500

    fix: prettier

--- src/navigation.js
+++ src/navigation.js
@@ -51,13 +51,33 @@ export const headerData = {
 
 export const footerData = {
   socialLinks: [
-    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: 'https://chat.whatsapp.com/EzYAadvUWyVBHt3m1FU77U', target: '_blank' },
+    {
+      ariaLabel: 'WhatsApp',
+      icon: 'tabler:brand-whatsapp',
+      href: 'https://chat.whatsapp.com/EzYAadvUWyVBHt3m1FU77U',
+      target: '_blank',
+    },
     { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: 'https://t.me/PerTechTalks', target: '_blank' },
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/PerTechTalks', target: '_blank' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/pertechtalks/', target: '_blank'},
-    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@pereiratechtalks', target: '_blank' },
+    {
+      ariaLabel: 'Instagram',
+      icon: 'tabler:brand-instagram',
+      href: 'https://www.instagram.com/pertechtalks/',
+      target: '_blank',
+    },
+    {
+      ariaLabel: 'Youtube',
+      icon: 'tabler:brand-youtube',
+      href: 'https://www.youtube.com/@pereiratechtalks',
+      target: '_blank',
+    },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pereira-tech-talks/', target: '_blank' },
+    {
+      ariaLabel: 'Github',
+      icon: 'tabler:brand-github',
+      href: 'https://github.com/pereira-tech-talks/',
+      target: '_blank',
+    },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
