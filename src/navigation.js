0c03f096256e6f5ab6d77d22efaeca056674fac1
Santiago Bernal <exactlimon@gmail.com>
2024-07-11 20:18:09 -0500

    feat: changed main and footer menu

--- src/navigation.js
+++ src/navigation.js
@@ -3,89 +3,61 @@ import { getPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'INICIO',
+      text: 'Inicio',
       href: '/',
     },
     {
-      text: 'COMUNIDAD',
-      href: '/sobre-la-comunidad-pereira-tech-talks',
+      text: 'Comunidad',
+      href: '/about',
     },
     {
-      text: 'MEETUPS',
-      href: 'https://www.meetup.com/es-ES/Pereira-Tech-Talks/',
+      text: 'Meetups',
+      href: 'https://www.meetup.com/es-ES/Pereira-Tech-Talks',
     },
     {
-      text: 'CALENDARIO',
-      href: '/calendario',
+      text: 'Blog',
+      href: getBlogPermalink(),
     },
     {
-      text: 'WHATSAPP',
-      href: 'https://chat.whatsapp.com/EzYAadvUWyVBHt3m1FU77U',
-    },
-    {
-      text: 'TELEGRAM',
-      href: 'https://t.me/PerTechTalks',
+      text: 'Contacto',
+      links: [
+        {
+          text: 'WhatsApp',
+          href: 'https://chat.whatsapp.com/EzYAadvUWyVBHt3m1FU77U',
+        },
+        {
+          text: 'Telegram',
+          href: 'https://t.me/PerTechTalks',
+        },
+        {
+          text: 'YouTube',
+          href: 'https://www.youtube.com/@pereiratechtalks',
+          target: '_blank',
+        },
+        {
+          text: 'Instagram',
+          href: 'https://www.instagram.com/pertechtalks/',
+          target: '_blank',
+        },
+        {
+          text: 'X (Twitter)',
+          href: 'https://x.com/PerTechTalks',
+          target: '_blank',
+        },
+      ],
     },
   ],
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/pertechtalks' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/pertechtalks' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/pertechtalks' },
+    { ariaLabel: 'WhatsApp', icon: 'tabler:brand-whatsapp', href: 'https://chat.whatsapp.com/EzYAadvUWyVBHt3m1FU77U', target: '_blank' },
+    { ariaLabel: 'Telegram', icon: 'tabler:brand-telegram', href: 'https://t.me/PerTechTalks', target: '_blank' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/PerTechTalks', target: '_blank' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/pertechtalks/', target: '_blank'},
+    { ariaLabel: 'Youtube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@pereiratechtalks', target: '_blank' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pereira-tech-talks' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pereira-tech-talks/', target: '_blank' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
