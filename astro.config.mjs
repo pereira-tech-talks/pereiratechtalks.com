127a977f862cdb4fd1b9815e779378b60def52b4
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2024-07-28 16:13:18 -0500

    Update ghpages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,6 +45,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   site: 'https://v2.pereiratechtalks.com',
+  base: '/docs',
   output: 'static',
   telemetry: false,
   server: {
