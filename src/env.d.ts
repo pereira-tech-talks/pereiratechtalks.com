564f1e184425410af5b6edb1e0a722fa9725885d
Mega-Barto <a.perez3@utp.edu.co>
2025-10-07 00:53:52 -0500

    Revert "feat: Add TypeScript declaration for JSON module support"
    This reverts commit 874a24ad9d89c4e29b193202f4c017fc10eda406.

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,3 @@
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
 /// <reference types="../integration/types.d.ts" />
-
-declare module '*.json' {
-  const value: any;
-  export default value;
-}
