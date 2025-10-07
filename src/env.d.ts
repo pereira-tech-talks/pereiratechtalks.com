874a24ad9d89c4e29b193202f4c017fc10eda406
Mega-Barto <a.perez3@utp.edu.co>
2025-10-07 00:50:43 -0500

    feat: Add TypeScript declaration for JSON module support

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,8 @@
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
 /// <reference types="../integration/types.d.ts" />
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
